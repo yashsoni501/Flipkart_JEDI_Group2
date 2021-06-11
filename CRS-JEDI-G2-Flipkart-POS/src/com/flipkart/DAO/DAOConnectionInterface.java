8495a0be7e6bae36ed0ecafd5a777ebba82b1857
yashsoni501 <yashsoni501@gmail.com>
2021-06-11 21:14:12 +0530

    update payment DAO

---
+++ CRS-JEDI-G2-Flipkart-POS/src/com/flipkart/DAO/DAOConnectionInterface.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.flipkart.DAO;
+
+import java.sql.Connection;
+
+/**
+ * @author yashsoni501
+ *
+ */
+public interface DAOConnectionInterface {
+
+	public static Connection conn = null;
+
+}
--- CRS-JEDI-G2-Flipkart-POS/src/com/flipkart/DAO/PaymentDAOInterface.java
+++ CRS-JEDI-G2-Flipkart-POS/src/com/flipkart/DAO/PaymentDAOInterface.java
@@ -3,10 +3,17 @@
  */
 package com.flipkart.DAO;
 
+import java.sql.SQLException;
+
+import com.flipkart.bean.Payment;
+
 /**
  * @author aysh
  *
  */
 public interface PaymentDAOInterface {
 
+	public Payment getFeeReciept(String studentId, int semester) throws SQLException;
+	public String onlinePayment(String studentId, float amount, int semester) throws SQLException;
+	public String offlinePayment(String studentId, float amount, int semester) throws SQLException;
 }
