1339fd9ae0f19aad833221a353c34a34df9d6de0
Himanshu Airan <62210670+Himanshu-77@users.noreply.github.com>
2021-06-11 09:30:28 +0530

    student menu added

---
+++ CRS-JEDI-G2-Flipkart-POS/src/com/flipkart/application/StudentCRSMenu.java
@@ -0,0 +1,82 @@
+/**
+ * 
+ */
+package com.flipkart.application;
+ 
+/**
+ * @author Aeron
+ *
+ */
+public class StudentCRSMenu {
+
+	StudentInterface tudentInterface = StudentServiceImpl.getInstance();
+	
+	/** Creates the menu for Students
+	 */
+	public void create_menu( ) {
+	
+			if(studentInterface.checkVerification( )) {
+				while(true) {
+					System.out.println("\n----------Student Menu-----------");
+					System.out.println("1. View registered courses");
+					System.out.println("2. Check if Fee is Paid");
+					System.out.println("3. View Fee receipt");
+					System.out.println("4. View report card");
+					System.out.println("5. Pay fees");
+					System.out.println("6. Register in courses");
+					
+					int optionChoosed = io.input.nextInt();
+					io.input.nextLine();
+					switch(optionChoosed) {
+						case 1:
+							fetchRegisteredCourses();
+							break;
+						case 2:
+							isFeePaid();
+							break;
+						case 3:
+							viewFeeReciept();
+							break;
+						case 4:
+							viewReportCard();
+							break;
+						case 5: 
+							payfee();
+							break;
+						case 6:
+							registerCourses();
+							break;
+						default:
+                            "Please enter valid option";
+					}
+				}
+			}
+			else {
+				System.out.println("Profile not verified");
+			}
+	}
+	
+	private void fetchRegisteredCourses() {
+
+    }
+
+    private void isFeePaid() {
+
+    }
+
+    private void viewFeeReciept() {
+
+    }
+
+    private void viewReportCard() {
+        
+    }
+
+    private void payfee() {
+        
+    }
+
+    private void registerCourses() {
+        
+    }	 
+}
