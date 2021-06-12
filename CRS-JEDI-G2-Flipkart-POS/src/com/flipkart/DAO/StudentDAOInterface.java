c1948ede9ab97fa8a977552eb9697116b3aaad65
Himanshu Airan <hagarwal056@gmail.com>
2021-06-12 22:46:08 +0530

    student dao added

--- CRS-JEDI-G2-Flipkart-POS/src/com/flipkart/DAO/StudentDAOInterface.java
+++ CRS-JEDI-G2-Flipkart-POS/src/com/flipkart/DAO/StudentDAOInterface.java
@@ -3,10 +3,24 @@
  */
 package com.flipkart.DAO;
 
+import java.util.ArrayList;
+
+import com.flipkart.bean.Student;
+import com.flipkart.bean.Course;
+
 /**
- * @author aysh
+ * @author Aeron
  *
  */
-public interface StudentDAOInterface {
+public class StudentDAOInterface {
+	
+	public ArrayList<Course> fetchRegisteredCourses(String studentId, int semester);
 
+	public boolean payFee(String studentId, int modeOfPayment);
+
+	public boolean isFeePaid(String studentId, int semester);
+
+	public ArrayList<Student> getAllStudents(String session);
+
+	public Student getStudentById(String userId);
 }
