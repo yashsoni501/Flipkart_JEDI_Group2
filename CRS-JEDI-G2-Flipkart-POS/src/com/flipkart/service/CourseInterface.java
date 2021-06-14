0cda9da8e41031834d6ab6b45662653ba972c6ab
TANMAY912 <tanudim2012@gmail.com>
2021-06-14 16:09:17 +0530

    Exceptions in CourseDAO and sevice level added

--- CRS-JEDI-G2-Flipkart-POS/src/com/flipkart/service/CourseInterface.java
+++ CRS-JEDI-G2-Flipkart-POS/src/com/flipkart/service/CourseInterface.java
@@ -6,13 +6,14 @@ package com.flipkart.service;
 import java.util.ArrayList;
 
 import com.flipkart.bean.Course;
+import com.flipkart.exception.CourseNotFoundException;
 
 /**
  * @author Lenovo
  *
  */
 public interface CourseInterface {
-	public Course getCourse(String courseId);
+	public Course getCourse(String courseId) throws CourseNotFoundException;
 
 	public ArrayList<Course> getAllCourses();
 }
